5ad806818964e24799639c4b3bace962c5de6795
Anjan Koradia <anjankoradia2101@gmail.com>
2020-12-03 18:45:34 +0530

    Live-classes  & Jee-Neet preparation section created.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(".jee_neet_preparation").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  fade: true,
+  cssEase: "linear",
+  prevArrow: "none",
+  nextArrow: "none",
+});

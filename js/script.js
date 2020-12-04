635c51b3a6aa06d5be8d6620d5c762b400738928
Anjan Koradia <anjankoradia2101@gmail.com>
2020-12-04 12:19:00 +0530

    Export Tutor & Student Comments section Created.

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ $(".jee_neet_preparation").slick({
   prevArrow: "none",
   nextArrow: "none",
 });
+
+$(".student_opinion").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  fade: true,
+  cssEase: "linear",
+  prevArrow: "none",
+  nextArrow: "none",
+});
